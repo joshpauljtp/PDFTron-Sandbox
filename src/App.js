90cd496dadcd9f62afdbab29c67b75d6ddf34d16
joshpauljtp <joshua@1stmain.co>
2022-05-24 18:40:56 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
-import React, { useRef, useEffect } from 'react';
-import WebViewer from '@pdftron/webviewer';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import WebViewer from "@pdftron/webviewer";
+import "./App.css";
 
 const App = () => {
   const viewer = useRef(null);
 
-  // if using a class, equivalent of componentDidMount 
+  // if using a class, equivalent of componentDidMount
   useEffect(() => {
     WebViewer(
       {
-        path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
+        path: "/webviewer/lib",
+        initialDoc: "/files/PDFTRON_about.pdf",
+        fullAPI: true,
       },
-      viewer.current,
+      viewer.current
     ).then((instance) => {
       const { documentViewer, annotationManager, Annotations } = instance.Core;
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
+      instance.UI.disableElements([
+        "leftPanel",
+        "leftPanelButton",
+        "viewControls",
+        "toolbarGroup-Annotate",
+        "toolbarGroup-Insert",
+        "toolbarGroup-Edit",
+        "toolbarGroup-Shapes",
+        "toolbarGroup-FillAndSign",
+        "signatureFieldToolGroupButton",
+        "radioButtonFieldToolGroupButton",
+        "listBoxFieldToolGroupButton",
+        "comboBoxFieldToolGroupButton",
+        "toolsOverlay",
+        "viewControlsButton",
+      ]);
 
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
-      });
+      //Styling Checkbox
+      Annotations.WidgetAnnotation.getCustomStyles = (widget) => {
+        if (widget instanceof Annotations.CheckButtonWidgetAnnotation) {
+          return {
+            "background-color": "blue",
+            color: "white",
+            opacity: 0.8,
+          };
+        }
+      };
     });
   }, []);
 
   return (
     <div className="App">
-      <div className="header">React sample</div>
       <div className="webviewer" ref={viewer}></div>
     </div>
   );
